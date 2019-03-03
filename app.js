c28c68d3e5fd505e18b50a51df23f13c4c2fee39
sayeed-ahmed-rasel <asayeed303@gmail.com>
2019-03-03 19:51:25 +0600

    test

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 
 
-
+app.get('/', (req, res) => {
+    res.send('welcome to express')
+})
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
-    console.log(`server listeing on port${port}`);
+    console.log(`server listeing on port ${port}`);
 })
